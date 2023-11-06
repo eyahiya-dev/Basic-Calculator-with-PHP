77cc404b5369979de80ac0833b488f7ea529e071
Yahya Mahmud <122890225+eyahiya-dev@users.noreply.github.com>
2023-11-06 23:16:11 +0600

    add html file for calculator

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <title>Basic Calculator with PHP</title>
+</head>
+<body>
+  <div class="h-screen flex justify-center items-center">
+    
+  </div>
+</body>
+</html>

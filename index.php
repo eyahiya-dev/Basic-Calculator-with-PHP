d1f8ab45345482f9334cb87c4715ef22ec909827
Yahya Mahmud <122890225+eyahiya-dev@users.noreply.github.com>
2023-11-06 23:27:07 +0600

    add tailwind  file for calculator

--- index.php
+++ index.php
@@ -7,8 +7,22 @@
   <title>Basic Calculator with PHP</title>
 </head>
 <body>
-  <div class="h-screen flex justify-center items-center">
-    
+  <div class="h-screen flex justify-center items-center">    
+    <div class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-7 dark:bg-gray-800 dark:border-gray-700">
+        <form class="space-y-6" action="#">
+            <h5 class="text-xl font-medium text-gray-900 dark:text-white text-center">Simple Calculator</h5>
+            <div>                
+                <input type="text" name="numberOne" class="bg-gray-50 border w-full p-2.5 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Enter Numbers" required>
+            </div>
+            <div>                
+                <input type="text" name="numberTwo" placeholder="Enter Numbers" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
+            </div>            
+            <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>            
+        </form>
+        <div class="text-sm text-center pt-10 font-medium text-gray-500 dark:text-gray-300">
+                Your mathmetical result is in below
+    </div>
+    </div>    
   </div>
 </body>
 </html>
